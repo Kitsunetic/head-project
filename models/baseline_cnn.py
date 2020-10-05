a031e8aa9d2f80f5ac9b0c99ae9e2166d5caa249
Kitsunetic <1996.jh.shim@gmail.com>
2020-10-05 10:33:09 +0900

    Add baseline models

---
+++ models/baseline_cnn.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class BaselineCNN1d(nn.Module):
+    def __init__(self, in_channels, out_channels, ):
+        super(BaselineCNN1d, self).__init__()
+
+        self.conv_in = nn.Conv1d()
+
+    def forward(self, x):
+        pass
+
+
