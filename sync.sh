1ec0b365561ab3b61ee4f6c29016a7d0878e08f3
Kitsunetic <kitsune.guru@gmail.com>
2020-11-23 12:37:50 +0900

    CLSTM

---
+++ sync.sh
@@ -0,0 +1,5 @@
+rclone copy checkpoint notebook:/head-project/checkpoint -Pv
+rclone copy notebook:/head-project/checkpoint checkpoint -Pv
+rclone copy data notebook:/head-project/data -Pv
+rclone copy notebook:/head-project/data data -Pv
+
